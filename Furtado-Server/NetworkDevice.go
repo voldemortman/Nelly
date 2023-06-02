0e30a89472ef2c99b3903689435b73cdb3d0d9c7
Daniel Mortman <danielmortman@gmail.com>
2023-06-02 19:46:21 +0300

    Create functions to send data to device, and close device

--- Furtado-Server/NetworkDevice.go
+++ Furtado-Server/NetworkDevice.go
@@ -1,9 +1,54 @@
 package furtado
 
 import (
+	"errors"
+	"fmt"
+
+	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 )
 
-type DeviceCommunicator struct {
-	Handle *pcap.Handle
+type NetworkDevice struct {
+	handle      *pcap.Handle
+	IsRunning   bool
+	stopChannel chan bool
+}
+
+func (device *NetworkDevice) ReadFromDevice() (chan *gopacket.Packet, error) {
+	if device.IsRunning {
+		return nil, errors.New("Device is already in use")
+	}
+	device.IsRunning = true
+
+	packetChan := make(chan *gopacket.Packet)
+	go func() {
+		for {
+			select {
+			case <-device.stopChannel:
+				close(packetChan)
+				return
+			default:
+				packetData, _, err := device.handle.ReadPacketData()
+				if err != nil {
+					fmt.Println("Error while reading packet: ", err)
+					return
+				}
+				packet := gopacket.NewPacket(packetData, device.handle.LinkType(), gopacket.Default)
+				packetChan <- &packet
+			}
+		}
+	}()
+	return packetChan, nil
+}
+
+func (device *NetworkDevice) SendToDevice(packetData []byte) {
+	err := device.handle.WritePacketData(packetData)
+	if err != nil {
+		fmt.Println("Error while writing packet to device: ", err)
+	}
+}
+
+func (device *NetworkDevice) CloseCommunication() {
+	device.stopChannel <- true
+	device.handle.Close()
 }
