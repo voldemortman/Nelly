7842d1099883bbafad15432a9c2bd596ab36368b
Daniel Mortman <danielmortman@gmail.com>
2023-06-03 19:26:05 +0300

    Create serialization utils

---
+++ furtado/pkg/netUtils/serialization.go
@@ -0,0 +1,29 @@
+package netUtils
+
+import (
+	"bytes"
+	"encoding/gob"
+
+	"github.com/google/gopacket"
+)
+
+func SerializePacket(packet gopacket.Packet) ([]byte, error) {
+	var buf bytes.Buffer
+	encoder := gob.NewEncoder(&buf)
+	err := encoder.Encode(packet)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func DeserializePacket(data []byte) (gopacket.Packet, error) {
+	var packet gopacket.Packet
+	buf := bytes.NewBuffer(data)
+	decoder := gob.NewDecoder(buf)
+	err := decoder.Decode(&packet)
+	if err != nil {
+		return nil, err
+	}
+	return packet, nil
+}
